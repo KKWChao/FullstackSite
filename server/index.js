a937dbd2eb3bed72d0053fcc7b8f18dd99c1815d
Kelvin Chao <kelvinchao2011@gmail.com>
2023-01-22 05:51:51 -0800

    working on post routes

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/user.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controller/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
@@ -44,11 +47,14 @@ const upload = multer({ storage });
 
 /* ROUTES W/ FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 // prefix for login
 // will contain verify token for routes
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;

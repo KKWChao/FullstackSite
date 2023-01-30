dd43637670a40c10da43cafd4b46c4acd3659643
Kelvin Chao <kelvinchao2011@gmail.com>
2023-01-30 01:14:50 -0800

    front end start

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -36,7 +36,7 @@ export const getUserFriends = async (req, res) => {
 /* UPDATE */
 export const addRemoveFriend = async (req, res) => {
   try {
-    const { eid, friendId } = req.params;
+    const { id, friendId } = req.params;
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
--- server/index.js
+++ server/index.js
@@ -11,9 +11,12 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import { register } from "./controller/auth.js";
+import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
@@ -65,5 +68,10 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONLY ONE TIME */
+    // Inserting data
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((err) => console.log(`${err} did not connect`));
